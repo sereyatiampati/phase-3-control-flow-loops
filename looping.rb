8d7e655cb16ac122d285f80af01f06a89e453181
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-02-08 08:24:58 +0300

    happy new year test passing

--- looping.rb
+++ looping.rb
@@ -1,7 +1,14 @@
+require 'pry'
 def happy_new_year
   # your code here
+  count =10
+  until count==0
+    puts "#{count}"
+    count -= 1
+  end
+  puts "Happy New Year!"
 end
-
+#binding.pry
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
